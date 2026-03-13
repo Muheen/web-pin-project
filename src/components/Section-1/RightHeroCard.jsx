9c1e4d8b21826188220eb3dca1f5d5ffa69339f0
Muheen Arshad <m.moheen999@gmail.com>
2026-03-14 01:40:47 +0500

    Project updated

--- src/components/Section-1/RightHeroCard.jsx
+++ src/components/Section-1/RightHeroCard.jsx
@@ -3,24 +3,22 @@ import { ArrowRight } from 'lucide-react'
 
 const RightHeroCard = (props) => {
   return (
-    <div className="h-full rounded-4xl overflow-hidden shrink-0 relative w-60">
+    <div className="h-full w-60 rounded-4xl overflow-hidden shrink-0 relative">
         <img className='h-full w-full object-cover' src={props.image} alt="img" />
 
+        {/* overlay shadow div */}
         <div className='h-[70%] p-6 w-full absolute bottom-0 left-0 bg-linear-to-t from-black/90 to-transparent'></div>
 
          <div className='h-full p-6 w-full absolute top-0 left-0 flex flex-col justify-between'>
-
-            <h3 className='text-xl bg-blue-500 w-8 h-8 flex justify-center items-center rounded-full'>1</h3>
+            <h3 className='text-xl bg-white w-8 h-8 flex justify-center items-center rounded-full'>{props.id}</h3>
             <div>
-              <p className='text-l mb-10 text-white'>{props.intro}</p>
-
-              <div className='flex items-center mb-2 text-white'>
-                <button className='text-xl mr-4 bg-blue-500 pt-1 pb-2 px-4 rounded-3xl'>{props.tag}
-                </button>
-                <span className=' bg-blue-500 py-2 px-2 rounded-3xl'><ArrowRight  size={24} strokeWidth={1.5} /></span>
-              </div>
+                <p className='text-l mb-10 text-white'>{props.intro}</p>
+                <div className='flex items-center mb-2 text-white'>
+                  <button className='text-xl mr-4 bg-blue-500 pt-1 pb-2 px-4 rounded-3xl'>{props.tag}
+                  </button>
+                  <span className=' bg-blue-500 py-2 px-2 rounded-3xl'><ArrowRight  size={24} strokeWidth={1.5} /></span>
+                </div>
             </div>
-
          </div>
       </div>
   )
--- src/components/Section-1/RightTextHero.jsx
+++ src/components/Section-1/RightTextHero.jsx
@@ -2,9 +2,9 @@ import RightHeroCard from "./RightHeroCard"
 
 const RightTextHero = (props) => {
   return (
-    <div className='h-full pl-4 gap-6 py-8 w-2/3 flex flex-nowrap overflow-x-auto'>
-      {props.users.map(function(el){
-        return <RightHeroCard image={el.image} tag={el.tag} intro={el.intro}/>
+    <div id="right" className='h-full pl-4 gap-6 pt-8 pb-12 w-2/3 flex flex-nowrap overflow-x-auto'>
+      {props.users.map(function(el,idx){
+        return <RightHeroCard id={idx+1} image={el.image} tag={el.tag} intro={el.intro}/>
       })}
     </div>
   )
